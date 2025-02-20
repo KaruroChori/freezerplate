182a378a48ee2ca6fcfa47d42e748b3673c478a3
karurochari <KaruroChori@users.noreply.github.com>
2025-02-20 17:03:20 +0000

    Moved all global variables in a context.

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <ostream>
 #include <set>
 #include <sstream>
 #include <string_view>
@@ -41,12 +42,24 @@ static std::vector<uint8_t> deflate(std::basic_string_view<uint8_t> data) {
 }
 #endif
 
+namespace fs = std::filesystem;
+
+struct ctx_t {
+  std::ofstream out;
+  std::ostringstream fn_table;
+  const char *LBRACE = "<?";
+  const size_t LBRACE_len = strlen(LBRACE);
+  const char *RBRACE = "?>";
+  const size_t RBRACE_len = strlen(RBRACE);
+  fs::path BASE;
+
+  ctx_t(fs::path name) : out(name, std::ios::binary) {}
+};
+
 extern std::string escape_c_str(const std::string &input);
 extern std::string escape_meson_str(const std::string &input);
 extern std::string escape_xml_str(const std::string &input);
 extern bool no_override;
-
-namespace fs = std::filesystem;
 
 // Define the writer status enumeration
 typedef enum {
@@ -61,17 +74,9 @@ typedef writer_status_t (*writer_t)(const char *, size_t);
 // Define the environment structure
 typedef pugi::xml_node env_t;
 
-const char *LBRACE = "<?";
-const size_t LBRACE_len = strlen(LBRACE);
-const char *RBRACE = "?>";
-const size_t RBRACE_len = strlen(RBRACE);
-
-fs::path BASE;
-
 // File templates
 std::pair<writer_status_t, std::string>
-generate_file_function(std::ostream &out, std::ostream &fn_table,
-                       const fs::path &source) {
+generate_file_function(ctx_t &ctx, const fs::path &source) {
   static int counter = 0;
 
   // Read file contents
@@ -88,33 +93,33 @@ generate_file_function(std::ostream &out, std::ostream &fn_table,
   file.close();
 
   std::string name = std::format("writer_file_{}", counter++);
-  out << std::format(
+  ctx.out << std::format(
       "writer_status_t {}(const fs::path& dir, const env_t& env, const "
       "std::set<fs::path>& exclude={{}} /*not used*/, const std::string& _name "
       "= {{}} /*not used*/){{\n",
       name);
-  fn_table << std::format(R"(fs_tree.emplace("{}",{});)",
-                          escape_c_str(fs::relative(source, BASE)), name);
-  out << std::format("static constexpr int perms = {};",
-                     (int)fs::status(source).permissions());
-  out << std::format("static constexpr const char* name  = \"{}\";\n",
-                     escape_c_str(source.filename()));
-  out << "fs::path file = dir / (_name.length()!=0?_name.c_str():name);\n";
-  out << "if(fs::exists(file) && no_override){std::cerr<<\"File \"<<file<<\" "
-         "already there. "
-         "It will not be overwritten by rule "
-      << name << ".\\n\";return WRITER_STATUS_SKIP;}\n";
-  out << "std::ofstream out(file, std::ios::binary);\n";
+  ctx.fn_table << std::format(R"(fs_tree.emplace("{}",{});)",
+                              escape_c_str(fs::relative(source, ctx.BASE)),
+                              name);
+  ctx.out << std::format("static constexpr int perms = {};",
+                         (int)fs::status(source).permissions());
+  ctx.out << std::format("static constexpr const char* name  = \"{}\";\n",
+                         escape_c_str(source.filename()));
+  ctx.out << "fs::path file = dir / (_name.length()!=0?_name.c_str():name);\n";
+  ctx.out << "if(fs::exists(file) && no_override){std::cerr<<\"File "
+             "\"<<file<<\" already there. It will not be overwritten by rule "
+          << name << ".\\n\";return WRITER_STATUS_SKIP;}\n";
+  ctx.out << "std::ofstream out(file, std::ios::binary);\n";
   bool mode = 0;
   uint base = 0;
   uint offset = 0;
   for (; offset < buffer.size();) {
     if (mode == 0 &&
-        ((buffer.size() - offset >= LBRACE_len &&
-          memcmp(buffer.data() + offset, LBRACE, LBRACE_len) == 0) ||
+        ((buffer.size() - offset >= ctx.LBRACE_len &&
+          memcmp(buffer.data() + offset, ctx.LBRACE, ctx.LBRACE_len) == 0) ||
          offset == buffer.size() - 1)) {
-      out << "{";
-      out << "constexpr const uint8_t tmp[] = {";
+      ctx.out << "{";
+      ctx.out << "constexpr const uint8_t tmp[] = {";
       // To get the last one
       if (offset == buffer.size() - 1)
         offset++;
@@ -127,9 +132,9 @@ generate_file_function(std::ostream &out, std::ostream &fn_table,
 #endif
       for (auto c : data) {
         auto tmp = std::format("{:#X},", c);
-        out.write(tmp.data(), tmp.size());
+        ctx.out.write(tmp.data(), tmp.size());
       }
-      out <<
+      ctx.out <<
           R"(};
 #ifdef TE4_COMPRESS
     auto dtmp = inflate({tmp,sizeof(tmp)});
@@ -140,30 +145,29 @@ generate_file_function(std::ostream &out, std::ostream &fn_table,
 })";
       mode = 1;
       if (offset < buffer.size())
-        offset += LBRACE_len;
+        offset += ctx.LBRACE_len;
       else
         break;
       base = offset;
       continue;
-    } else if (mode == 1 &&
-               ((buffer.size() - offset >= RBRACE_len &&
-                 memcmp(buffer.data() + offset, RBRACE, RBRACE_len) == 0) ||
-                offset == buffer.size())) {
+    } else if (mode == 1 && ((buffer.size() - offset >= ctx.RBRACE_len &&
+                              memcmp(buffer.data() + offset, ctx.RBRACE,
+                                     ctx.RBRACE_len) == 0) ||
+                             offset == buffer.size())) {
       // To get the last one
       if (offset == buffer.size() - 1)
         offset++;
-      out.write((const char *)buffer.data() + base, offset - base);
+      ctx.out.write((const char *)buffer.data() + base, offset - base);
 
       mode = 0;
       if (offset < buffer.size()) {
-        offset += RBRACE_len;
+        offset += ctx.RBRACE_len;
         // Skip empty lines generated by directives
-        if (((base > LBRACE_len && buffer[base - LBRACE_len - 1] == '\n') ||
-             base == LBRACE_len) &&
+        if (((base > ctx.LBRACE_len &&
+              buffer[base - ctx.LBRACE_len - 1] == '\n') ||
+             base == ctx.LBRACE_len) &&
             ((offset < buffer.size() && buffer[offset] == '\n') ||
              offset == buffer.size())) {
-          std::cout.write((const char *)buffer.data() + base, offset - base);
-          std::cout << ".....\n";
           offset++;
         }
       } else
@@ -175,16 +179,14 @@ generate_file_function(std::ostream &out, std::ostream &fn_table,
       offset++;
     }
   }
-  out << "out.close();\n"
-         "fs::permissions(file,fs::perms(perms), fs::perm_options::replace);\n"
-         "return WRITER_STATUS_OK;\n}\n";
+  ctx.out << "out.close();\nfs::permissions(file,fs::perms(perms), "
+             "fs::perm_options::replace);\nreturn WRITER_STATUS_OK;\n}\n";
   return {WRITER_STATUS_OK, name};
 }
 
 // Mostly binary files which should not be edited but still copied.
 std::pair<writer_status_t, std::string>
-generate_raw_function(std::ostream &out, std::ostream &fn_table,
-                      const fs::path &source) {
+generate_raw_function(ctx_t &ctx, const fs::path &source) {
   static int counter = 0;
 
   // Read file contents
@@ -201,26 +203,27 @@ generate_raw_function(std::ostream &out, std::ostream &fn_table,
   file.close();
 
   std::string name = std::format("writer_raw_{}", counter++);
-  out << std::format(
+  ctx.out << std::format(
       "writer_status_t {}(const fs::path& dir, const env_t& env, const "
       "std::set<fs::path>& exclude={{}} /*not used*/, const std::string& _name "
       "= {{}} /*not used*/){{\n",
       name);
-  fn_table << std::format(R"(fs_tree.emplace("{}",{});)",
-                          escape_c_str(fs::relative(source, BASE)), name);
-  out << std::format("static constexpr int perms = {};",
-                     (int)fs::status(source).permissions());
-  out << std::format("static constexpr const char* name  = \"{}\";\n",
-                     escape_c_str(source.filename()));
-  out << "fs::path file = dir / (_name.length()!=0?_name.c_str():name);\n";
-  out << "if(fs::exists(file) && no_override){std::cerr<<\"File \"<<file<<\" "
-         "already there. "
-         "It will not be overwritten by rule "
+  ctx.fn_table << std::format(R"(fs_tree.emplace("{}",{});)",
+                              escape_c_str(fs::relative(source, ctx.BASE)),
+                              name);
+  ctx.out << std::format("static constexpr int perms = {};",
+                         (int)fs::status(source).permissions());
+  ctx.out << std::format("static constexpr const char* name  = \"{}\";\n",
+                         escape_c_str(source.filename()));
+  ctx.out << "fs::path file = dir / (_name.length()!=0?_name.c_str():name);\n";
+  ctx.out
+      << "if(fs::exists(file) && no_override){std::cerr<<\"File \"<<file<<\" "
+         "already there. It will not be overwritten by rule "
       << name << ".\\n\";return WRITER_STATUS_SKIP;}\n";
-  out << "std::ofstream out(file, std::ios::binary);\n";
+  ctx.out << "std::ofstream out(file, std::ios::binary);\n";
 
-  out << "{";
-  out << "constexpr const uint8_t tmp[] = {";
+  ctx.out << "{";
+  ctx.out << "constexpr const uint8_t tmp[] = {";
 #ifdef TE4_COMPRESS
   auto data =
       deflate(std::basic_string_view<uint8_t>(buffer.begin(), buffer.end()));
@@ -229,9 +232,9 @@ generate_raw_function(std::ostream &out, std::ostream &fn_table,
 #endif
   for (auto c : data) {
     auto tmp = std::format("{:#X},", c);
-    out.write(tmp.data(), tmp.size());
+    ctx.out.write(tmp.data(), tmp.size());
   }
-  out <<
+  ctx.out <<
       R"(};
 #ifdef TE4_COMPRESS
     auto dtmp = inflate({tmp,sizeof(tmp)});
@@ -241,37 +244,40 @@ generate_raw_function(std::ostream &out, std::ostream &fn_table,
     out.write((const char*)dtmp.data(),dtmp.size());
 })";
 
-  out << "out.close();\n"
+  ctx.out
+      << "out.close();\n"
          "fs::permissions(file,fs::perms(perms), fs::perm_options::replace);\n"
          "return WRITER_STATUS_OK;\n}\n";
   return {WRITER_STATUS_OK, name};
 }
 
 std::pair<writer_status_t, std::string>
-generate_link_function(std::ostream &out, std::ostream &fn_table,
-                       const fs::path &source) {
+generate_link_function(ctx_t &ctx, const fs::path &source) {
   static int counter = 0;
   std::string name = std::format("writer_link_{}", counter++);
-  out << std::format(
+  ctx.out << std::format(
       "writer_status_t {}(const fs::path& dir, const env_t& env, const "
       "std::set<fs::path>& exclude={{}} /*not used*/,  const std::string& "
       "_name = {{}} /*not used*/){{\n",
       name);
-  out << std::format("static constexpr int perms = {};",
-                     (int)fs::status(source).permissions());
-  fn_table << std::format(R"(fs_tree.emplace("{}",{});)",
-                          escape_c_str(fs::relative(source, BASE)), name);
-  out << std::format("static constexpr const char* name  = \"{}\";\n",
-                     escape_c_str(source.filename()));
-  out << "fs::path file = dir / (_name.length()!=0?_name.c_str():name);\n";
-  out << "if(fs::exists(file) && no_override){std::cerr<<\"File \"<<file<<\" "
+  ctx.out << std::format("static constexpr int perms = {};",
+                         (int)fs::status(source).permissions());
+  ctx.fn_table << std::format(R"(fs_tree.emplace("{}",{});)",
+                              escape_c_str(fs::relative(source, ctx.BASE)),
+                              name);
+  ctx.out << std::format("static constexpr const char* name  = \"{}\";\n",
+                         escape_c_str(source.filename()));
+  ctx.out << "fs::path file = dir / (_name.length()!=0?_name.c_str():name);\n";
+  ctx.out
+      << "if(fs::exists(file) && no_override){std::cerr<<\"File \"<<file<<\" "
          "already there. "
          "It will not be overwritten by rule "
       << name << ".\\n\";return WRITER_STATUS_SKIP;}\n";
-  out << "auto t = \""
-      << escape_c_str(fs::read_symlink(source).lexically_normal().string())
-      << "\";\n"; // TODO: escape
-  out << "fs::create_symlink(t,file);\n"
+  ctx.out << "auto t = \""
+          << escape_c_str(fs::read_symlink(source).lexically_normal().string())
+          << "\";\n"; // TODO: escape
+  ctx.out
+      << "fs::create_symlink(t,file);\n"
          "fs::permissions(file,fs::perms(perms), fs::perm_options::replace);\n"
          "return WRITER_STATUS_OK;\n}\n";
   return {WRITER_STATUS_OK, name};
@@ -279,8 +285,7 @@ generate_link_function(std::ostream &out, std::ostream &fn_table,
 
 // Function to generate folder functions
 std::pair<writer_status_t, std::string>
-generate_folder_function(std::ostream &out, std::ostream &fn_table,
-                         const fs::path &source,
+generate_folder_function(ctx_t &ctx, const fs::path &source,
                          const std::set<fs::path> &exclude = {},
                          const std::set<fs::path> &exclude_ext = {}) {
   static int counter = 0;
@@ -298,7 +303,7 @@ generate_folder_function(std::ostream &out, std::ostream &fn_table,
 
     // Special case to resolve symbolic links.
     if (entry.is_symlink()) {
-      auto t = generate_link_function(out, fn_table, entry);
+      auto t = generate_link_function(ctx, entry);
       if (t.first == WRITER_STATUS_ERROR)
         return {WRITER_STATUS_ERROR, ""};
       else
@@ -308,7 +313,7 @@ generate_folder_function(std::ostream &out, std::ostream &fn_table,
 
     // Folders are ignored in this context.
     if (entry.is_directory()) {
-      auto t = generate_folder_function(out, fn_table, entry, exclude);
+      auto t = generate_folder_function(ctx, entry, exclude);
       if (t.first == WRITER_STATUS_ERROR)
         return {WRITER_STATUS_ERROR, ""};
       else
@@ -318,7 +323,7 @@ generate_folder_function(std::ostream &out, std::ostream &fn_table,
 
     // Skip templating for selected extensions
     if (exclude_ext.find(entry.path().extension()) != exclude_ext.end()) {
-      auto t = generate_raw_function(out, fn_table, entry);
+      auto t = generate_raw_function(ctx, entry);
       if (t.first == WRITER_STATUS_ERROR)
         return {WRITER_STATUS_ERROR, ""};
       else
@@ -326,31 +331,33 @@ generate_folder_function(std::ostream &out, std::ostream &fn_table,
       continue;
     }
 
-    auto t = generate_file_function(out, fn_table, entry);
+    auto t = generate_file_function(ctx, entry);
     if (t.first == WRITER_STATUS_ERROR)
       return {WRITER_STATUS_ERROR, ""};
     else
       callthese.push_back(t.second);
   }
 
-  out << std::format(
+  ctx.out << std::format(
       "writer_status_t {}(const fs::path& dir, const env_t& env, const "
       "std::set<fs::path>& exclude={{}}, const std::string& _name = {{}}){{\n",
       name);
-  out << std::format("static constexpr int perms = {};",
-                     (int)fs::status(source).permissions());
-  fn_table << std::format(R"(fs_tree.emplace("{}",{});)",
-                          escape_c_str(fs::relative(source, BASE)), name);
-  out << std::format("static constexpr const char* name  = \"{}\";\n",
-                     escape_c_str(source.filename()));
-  out << "fs::path file = dir / (_name.length()!=0?_name.c_str():name);\n";
-  out << "fs::create_directories(file);\n";
+  ctx.out << std::format("static constexpr int perms = {};",
+                         (int)fs::status(source).permissions());
+  ctx.fn_table << std::format(R"(fs_tree.emplace("{}",{});)",
+                              escape_c_str(fs::relative(source, ctx.BASE)),
+                              name);
+  ctx.out << std::format("static constexpr const char* name  = \"{}\";\n",
+                         escape_c_str(source.filename()));
+  ctx.out << "fs::path file = dir / (_name.length()!=0?_name.c_str():name);\n";
+  ctx.out << "fs::create_directories(file);\n";
   for (auto fn : callthese) {
-    out << std::format(
+    ctx.out << std::format(
         "if(exclude.find(file)==exclude.end()){}(file, env, exclude);\n", fn);
   }
 
-  out << "fs::permissions(file,fs::perms(perms), fs::perm_options::replace);\n"
+  ctx.out
+      << "fs::permissions(file,fs::perms(perms), fs::perm_options::replace);\n"
       << "return WRITER_STATUS_OK;\n}\n";
 
   return {WRITER_STATUS_OK, name};
@@ -382,12 +389,11 @@ int main(int argc, char *argv[]) {
     std::cerr << "File " << file
               << " already there. It will not be overwritten\n";
   } else {
-    std::ofstream out(file, std::ios::binary);
-    std::ostringstream fn_table;
+    ctx_t ctx(file);
 #ifdef TE4_COMPRESS
-    out << "#ifndef TE4_INTERNAL\n#define TE4_COMPRESS\n#endif";
+    ctx.out << "#ifndef TE4_INTERNAL\n#define TE4_COMPRESS\n#endif";
 #endif
-    out <<
+    ctx.out <<
         R"(
 //Automatically generated, if you want to include changes, please do that in the main function
 //The rest should not be touched.
@@ -526,10 +532,10 @@ std::map<fs::path, writer_status_t(*)(const fs::path& dir, const env_t& env, con
     else
       doc.load_string("<project></project>");
 
-    BASE = argv[1];
+    ctx.BASE = argv[1];
 
-    generate_folder_function(out, fn_table, BASE, {});
-    out <<
+    generate_folder_function(ctx, ctx.BASE, {});
+    ctx.out <<
         R"(
 #ifndef TE4_INTERNAL
 int main(int argc, const char* argv[]){
@@ -537,8 +543,8 @@ int main(int argc, const char* argv[]){
     pugi::xml_document doc;
     doc.load_file(/*(fs::path(getenv("PWD"))/argv[2]).c_str()*/argv[2]);
 )";
-    out << fn_table.str();
-    out << R"(
+    ctx.out << ctx.fn_table.str();
+    ctx.out << R"(
     //Default entry point. Change by adding exclusions and further calls if you need to change its behaviour
     #if !__has_include("impl.frag.cpp")
 )";
@@ -546,15 +552,15 @@ int main(int argc, const char* argv[]){
     for (auto &entry : doc.child("project").child("steps").children()) {
       auto src = entry.attribute("source").as_string(nullptr);
       if (src != nullptr) {
-        out << std::format(
+        ctx.out << std::format(
             "fs_tree[\"{}\"](argv[1], doc.root(), {{}}, {{}});\n",
             escape_c_str(src));
       }
     }
     if (!(doc.child("project").child("steps"))) {
-      out << R"(        writer_dir_0(argv[1], doc.root());)" << "\n";
+      ctx.out << R"(        writer_dir_0(argv[1], doc.root());)" << "\n";
     }
-    out << R"(
+    ctx.out << R"(
     #else
         #include "impl.frag.cpp"
     #endif
@@ -562,12 +568,12 @@ int main(int argc, const char* argv[]){
 
     writer_dir_0(argv[2], doc.root());
 
-    out << R"(
+    ctx.out << R"(
     return 0;
 }
 #endif
 )";
-    out.close();
+    ctx.out.close();
   }
 
   return 0;
